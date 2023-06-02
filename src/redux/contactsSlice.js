c9460aeed1fc9f4c592c2dd15bf533ea8dc0fad1
Ann Dolbina <dolbinaann@gmail.com>
2023-06-02 10:08:05 +0300

    add builder

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -9,48 +9,48 @@ const handleRejected = (state, action) => {
   state.error = action.payload;
 };
 
+const fetchContactsFulfilledReducer = (state, action) => {
+  state.isLoading = false;
+  state.error = null;
+  state.items = action.payload;
+};
+
+const addContactFulfilledReducer = (state, action) => {
+  state.isLoading = false;
+  state.error = null;
+  state.items.push(action.payload);
+      
+};
+ 
+const deleteContactFulfilledReducer = (state, action) => {
+  state.isLoading = false;
+  state.error = null;
+  const index = state.items.findIndex(
+    contact => contact.id === action.payload.id
+  );
+  state.items.splice(index, 1);
+};
+
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
-      items: [],
-      isLoading: false,
-      error: null,
-    },
-    
-  
-  extraReducers: {
-    [fetchContacts.pending]: handlePending,
-    [addContact.pending]: handlePending,
-    [deleteContact.pending]: handlePending,
-    
-    [fetchContacts.rejected]: handleRejected,
-    [addContact.rejected]: handleRejected,
-    [deleteContact.rejected]: handleRejected,
-   
-
-    [fetchContacts.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items = action.payload;
-    },
-
-    [addContact.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items.push(action.payload);
-      
-    },
-
-    [deleteContact.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      const index = state.items.findIndex(
-        contact => contact.id === action.payload.id
-      );
-      state.items.splice(index, 1);
-    },
-     
+    items: [],
+    isLoading: false,
+    error: null,
   },
+
+  extraReducers: builder =>
+    builder
+      .addCase(fetchContacts.pending, handlePending)
+      .addCase(fetchContacts.fulfilled, fetchContactsFulfilledReducer)
+      .addCase(fetchContacts.rejected, handleRejected)
+      .addCase(addContact.pending, handlePending)
+      .addCase(addContact.fulfilled, addContactFulfilledReducer)
+      .addCase(addContact.rejected, handleRejected)
+      .addCase(deleteContact.pending, handlePending)
+      .addCase(deleteContact.fulfilled, deleteContactFulfilledReducer)
+      .addCase(deleteContact.rejected, handleRejected),
+ 
 });
 
   
